ae146b73b44befe3b839b0ece9ebb02f3f01dd7e
Johnny.Wang <johnny.wang@btse.com>
2024-07-08 11:59:04 +0800

    feat: three sum done

--- three_sum/template.go
+++ three_sum/template.go
@@ -1,22 +1,54 @@
 package three_sum
 
 import (
-	"go_leetcode_journey/common"
 	"sort"
 )
 
 func threeSum(nums []int) [][]int {
 	// 1. sort nums asc
 	sort.Ints(nums)
-	//fmt.Println("Sorted array:", nums)
+	numsLen := len(nums)
 
-	// forEach nums with i and make 2 pointer left = i + 1, right = len(nums) - 1
+	// 2. create 2D array for contain result
+	ans := make([][]int, 0)
+
+	// 3. forEach nums with i and make 2 pointer left = i + 1, right = len(nums) - 1
 	for i := range nums {
+		// 4. skip first elements duplicates:
+		if i > 0 && nums[i] == nums[i-1] {
+			continue
+		}
+
 		left := i + 1
-		right := len(nums) - 1
+		right := numsLen - 1
+
+		for left < right {
+			sumResult := nums[i] + nums[left] + nums[right]
+
+			if sumResult == 0 {
+				// append nums[i], nums[left], nums[right] to ans
+				ans = append(ans, []int{nums[i], nums[left], nums[right]})
+
+				// skip left and right duplicates
+				for left < right && nums[left] == nums[left+1] {
+					left += 1
+				}
+				for left < right && nums[right] == nums[right+-1] {
+					right -= 1
+				}
+				left++
+				right--
+			} else if sumResult < 0 {
+				// move left idx to the right by 1
+				left += 1
+			} else {
+				// move right idx to the left by 1
+				right -= 1
+			}
+		}
 	}
 
-	return nil
+	return ans
 }
 
 // Go call this func in main.go
@@ -30,5 +62,5 @@ func Go() {
 	ans1[1][0] = -1
 	ans1[1][1] = 0
 	ans1[1][2] = 1
-	common.Assert_answer(threeSum([]int{-1, 0, 1, 2, -1, -4}), ans1)
+	//common.Assert_answer(threeSum([]int{-1, 0, 1, 2, -1, -4}), ans1)
 }
