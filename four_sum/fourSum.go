3a3c4d2b55e174d1b2e25b94b0681ac22b3ecd3b
Johnny.Wang <johnny.wang@btse.com>
2024-07-28 15:19:36 +0800

    feat: fourSum done.

--- four_sum/fourSum.go
+++ four_sum/fourSum.go
@@ -1,10 +1,63 @@
 package four_sum
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 func fourSum(nums []int, target int) [][]int {
-	// TODO
-	return nil
+	// 1. sort nums asc
+	sort.Ints(nums)
+	return kSum(nums, target, 0, 4)
+}
+
+func kSum(nums []int, target int, start int, k int) [][]int {
+	res := make([][]int, 0)
+
+	if start == len(nums) {
+		return res
+	}
+
+	// 2. calculate avg with target and k
+	avg := target / k
+	// the num that we're judging, if max num is lower than avg or min num is bigger than avg, return res.
+	if nums[start] > avg || nums[len(nums)-1] < avg {
+		return res
+	}
+
+	if k == 2 {
+		return twoSum(nums, target, start)
+	}
+
+	for i := start; i < len(nums); i++ {
+		if i == start || nums[i] != nums[i-1] {
+			for _, subset := range kSum(nums, target-nums[i], i+1, k-1) {
+				res = append(res, append([]int{nums[i]}, subset...))
+			}
+		}
+	}
+
+	return res
+}
+
+func twoSum(nums []int, target int, start int) [][]int {
+	res := make([][]int, 0)
+	low := start
+	high := len(nums) - 1
+
+	for high > low {
+		tempSum := nums[low] + nums[high]
+		if tempSum < target || (low > start && nums[low] == nums[low-1]) {
+			low++
+		} else if tempSum > target || (high < len(nums)-1 && nums[high] == nums[high+1]) {
+			high--
+		} else {
+			res = append(res, []int{nums[low], nums[high]})
+			low++
+			high--
+		}
+	}
+	return res
 }
 
 func Go() {
