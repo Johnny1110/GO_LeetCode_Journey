d82394dd0719edec21a242b5260d9434c3b58264
Johnny.Wang <johnny.wang@btse.com>
2024-08-10 13:32:18 +0800

    feat: init Merge Two Sorted Lists

---
+++ merge_two_sorted_lists/template.go
@@ -0,0 +1,16 @@
+package merge_two_sorted_lists
+
+// Definition for singly-linked list.
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+	return nil
+}
+
+// Go call this func in main.go
+func Go() {
+
+}
