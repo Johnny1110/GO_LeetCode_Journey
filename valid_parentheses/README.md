13863641f0eecd70b2d6cc7714069625020ea900
Johnny.Wang <johnny.wang@btse.com>
2024-08-10 13:25:16 +0800

    feat: valid parentheses done

--- valid_parentheses/README.md
+++ valid_parentheses/README.md
@@ -50,3 +50,77 @@ Constraints:
 * String
 * Stack
 
+
+<br>
+
+## Thinking
+
+LeetCode Hint say using stack top resolve it.
+
+Iterate through all the char in input string, if encounter a open bracket like `( { [`, push em
+to the top of the stack, if encounter a close bracket like `) ] }` then check the top of the stack if it is
+a mapping open bracket.
+
+but I think I don't want to do it like that, what don't iterate though the input string then check i and i+1 ?
+
+let's try it out.
+
+<br>
+
+after like 10 mins later, I did it like this:
+
+```golang
+var bracketMap = map[rune]rune{
+	'(': ')',
+	'{': '}',
+	'[': ']',
+}
+
+func isValid(s string) bool {
+	//for _, c := range s {
+	//	fmt.Println(string(c))
+	//}
+
+	length := len(s)
+	if length%2 == 1 || length == 0 {
+		// if len is even, then there must have 1 bracket without another one.
+		return false
+	}
+	for i := 0; i < length; i++ {
+		c1 := rune(s[i])
+		c2 := rune(s[i+1])
+		if c2 != bracketMap[c1] {
+			return false
+		}
+		i = i + 1
+	}
+
+	return true
+}
+```
+
+<br>
+
+and there's a error:
+
+```
+Input
+s = "{[]}"
+
+Output: false
+Expected: true
+```
+
+<br>
+
+Oh... that's why leetcode give me hint using stack.
+
+<br>
+
+let's do it again.
+
+
+
+
+
+
--- valid_parentheses/template.go
+++ valid_parentheses/template.go
@@ -1,12 +1,77 @@
 package valid_parentheses
 
-import "go_leetcode_journey/common"
+import (
+	"go_leetcode_journey/common"
+)
 
-func isValid(s string) bool {
+var bracketMap = map[rune]rune{
+	')': '(',
+	'}': '{',
+	']': '[',
+}
+
+// Define the stack type
+type stack []rune
+
+// Push adds an element to the top of the stack
+func (s *stack) Push(v rune) {
+	*s = append(*s, v)
+}
+
+// Pop removes and returns the top element of the stack
+func (s *stack) Pop() (rune, bool) {
+	l := len(*s)
+	if l == 0 {
+		return 0, false // Return zero value and false if the stack is empty
+	}
+
+	// Get the last element
+	top := (*s)[l-1]
+
+	// Update the stack to remove the last element
+	*s = (*s)[:l-1]
+
+	return top, true
+}
+
+func isValid(input string) bool {
+	length := len(input)
+	if length%2 == 1 || length == 0 {
+		// if len is even, then there must have 1 bracket without another one.
+		return false
+	}
+	runeStack := stack{}
+
+	for _, c := range input {
+		if c == '(' || c == '[' || c == '{' {
+			runeStack.Push(c)
+		} else { // c == ) ] }
+			open, b := runeStack.Pop()
+			theOpenPair := bracketMap[c]
+			if b {
+				if open != theOpenPair {
+					return false
+				}
+			} else {
+				return false
+			}
+		}
+	}
+
+	_, check := runeStack.Pop()
+	if check {
+		return false
+	}
+
 	return true
 }
 
 // Go call this func in main.go
 func Go() {
-	common.Assert_answer(isValid("input"), true)
+	common.Assert_answer(isValid("{}()[}"), false)
+	common.Assert_answer(isValid("{}()}"), false)
+	common.Assert_answer(isValid("{}()[]"), true)
+	common.Assert_answer(isValid(""), false)
+	common.Assert_answer(isValid("{[]}"), true)
+	common.Assert_answer(isValid("{{"), false)
 }
