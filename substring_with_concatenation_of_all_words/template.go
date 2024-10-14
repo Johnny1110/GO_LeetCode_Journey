ae4e8ba375fa1fd4b0cb33b157dd6790bd4ee5d4
Johnny.Wang <johnny.wang@btse.com>
2024-10-14 18:48:00 +0800

    init: Substring with Concatenation of All Words

---
+++ substring_with_concatenation_of_all_words/template.go
@@ -0,0 +1,10 @@
+package substring_with_concatenation_of_all_words
+
+func findSubstring(s string, words []string) []int {
+	return []int{1, 2, 3}
+}
+
+// Go call this func in main.go
+func Go() {
+
+}
