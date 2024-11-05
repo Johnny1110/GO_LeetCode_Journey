3b3a8500760996f77781fc1112e9e3816e40357d
Johnny.Wang <johnny.wang@btse.com>
2024-11-05 16:07:27 +0800

    init: search_insert_position

---
+++ search_insert_position/template.go
@@ -0,0 +1,12 @@
+package search_insert_position
+
+import "go_leetcode_journey/common"
+
+func searchInsert(nums []int, target int) int {
+	return -1
+}
+
+// Go call this func in main.go
+func Go() {
+	common.Assert_answer(searchInsert([]int{1, 3, 5, 6}, 5), 2)
+}
