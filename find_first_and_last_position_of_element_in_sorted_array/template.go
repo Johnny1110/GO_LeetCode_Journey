52230d021fb71da7a0be705d89c0c317515bf14d
Johnny.Wang <johnny.wang@btse.com>
2024-11-05 17:12:32 +0800

    refactor: find_first_and_last_position_of_element_in_sorted_array

--- find_first_and_last_position_of_element_in_sorted_array/template.go
+++ find_first_and_last_position_of_element_in_sorted_array/template.go
@@ -6,47 +6,45 @@ import "fmt"
 func searchRange(nums []int, target int) []int {
 	leftmost := binSearchSideMost(nums, target, true)
 	rightmost := binSearchSideMost(nums, target, false)
-	//rightmost := -1
 	return []int{leftmost, rightmost}
 }
 
 func binSearchSideMost(nums []int, target int, leftmost bool) int {
 	left, right := 0, len(nums)-1
+	result := -1
+
 	for left <= right {
 
 		mid := left + (right-left)>>1
 
-		//fmt.Println("left:", left, "right:", right, "mid:", mid)
+		if nums[mid] == target {
+			result = mid
+
+			if leftmost {
+				right = mid - 1
+			} else {
+				left = mid + 1
+			}
+		}
 
 		if nums[mid] < target {
 			left = mid + 1
-		}
-		if nums[mid] > target {
+		} else if nums[mid] > target {
 			right = mid - 1
-		}
-
-		if nums[mid] == target {
-			if left == right {
-				return mid
-			}
-
-			if right-left == 1 {
-				if !leftmost && nums[right] == target {
-					return right
-				}
-				return mid
-			}
-
-			if leftmost {
-				right = mid
-			} else {
-				left = mid
-			}
 		}
 	}
 
-	return -1
+	return result
 }
+
+// ---------------------------------------------------------
+
+// searchRange Refactor
+// Adjust leftmost and rightmost Searches: When searching for the leftmost or rightmost position, you don’t need to check if left == right. The binary search logic will automatically converge to the desired boundary.
+// Refine the Conditionals: You don’t need the special cases where right - left == 1 since binary search with adjusted bounds will narrow in on the target.
+// Shift Bitwise Operation: mid := left + (right - left) >> 1 should have parentheses around the whole calculation (i.e., (right-left)>>1). However, it’s more readable to use (right + left) / 2 in Go, which is safe for integer calculations and avoids overflow.
+
+// ---------------------------------------------------------
 
 // Go call this func in main.go
 func Go() {
