6382905b68bfd92ccfbf42ef76c83198cfbbcc87
Johnny.Wang <johnny.wang@btse.com>
2024-11-06 16:29:36 +0800

    init :Valid Sudoku

---
+++ valid_sudoku/template.go
@@ -0,0 +1,12 @@
+package main
+
+import "go_leetcode_journey/common"
+
+func isValidSudoku(board [][]byte) bool {
+	return true
+}
+
+// Go call this func in main.go
+func Go() {
+	common.Assert_answer(isValidSudoku(nil), true)
+}
