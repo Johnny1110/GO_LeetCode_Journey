18c84c2e106c30c305bc452615bd33bfcb8264ff
Johnny.Wang <johnny.wang@btse.com>
2025-01-04 16:26:50 +0800

    feat: first_missing_positive done

--- first_missing_positive/template.go
+++ first_missing_positive/template.go
@@ -2,19 +2,26 @@ package first_missing_positive
 
 import (
 	"go_leetcode_journey/common"
-	"sort"
 )
 
 func firstMissingPositive(nums []int) int {
-	nums = append(nums, 0)
-	sort.Ints(nums)
-	for i := 0; i < len(nums)-1; i++ {
-		expectNext := nums[i] + 1
-		if expectNext < nums[i+1] && expectNext > 0 {
-			return expectNext
+	n := len(nums)
+	for i := 0; i < len(nums); i++ {
+		// ⬇ if number not in where it should be (nums[nums[i]-1]) .
+		for nums[i] > 0 && nums[i] <= n && nums[i] != nums[nums[i]-1] {
+			// Why using for ? because we swapped nums[i] to where it should be, then we got a new number in nums[i],
+			// we got to check new number again. until not match up with above judgmental.
+			nums[i], nums[nums[i]-1] = nums[nums[i]-1], nums[i]
 		}
 	}
-	return nums[len(nums)-1] + 1
+
+	for i := 0; i < len(nums); i++ {
+		if nums[i] != i+1 {
+			return i + 1
+		}
+	}
+
+	return n + 1
 }
 
 // Go call this func in main.go
@@ -38,4 +45,12 @@ func Go() {
 	example5 := []int{7, 8, 9, 11, 12}
 	ans5 := firstMissingPositive(example5)
 	common.Assert_answer(ans5, 1)
+
+	example6 := []int{-50, 0, 1, 3, 5, 100}
+	ans6 := firstMissingPositive(example6)
+	common.Assert_answer(ans6, 2)
+
+	example7 := []int{-1, 4, 2, 1, 9, 10}
+	ans7 := firstMissingPositive(example7)
+	common.Assert_answer(ans7, 3)
 }
