e8eeedb5f5793a6de21b896f923f0eacc5819fe7
Johnny.Wang <johnny.wang@btse.com>
2025-01-25 17:42:26 +0800

    init: Wildcard Matching

---
+++ hard/wildcard_matching/template.go
@@ -0,0 +1,13 @@
+package wildcard_matching
+
+import "go_leetcode_journey/common"
+
+func isMatch(s string, p string) bool {
+	// TODO
+	return false
+}
+
+// Go call this func in main.go
+func Go() {
+	common.Assert_answer(isMatch("input", "input"), true)
+}
