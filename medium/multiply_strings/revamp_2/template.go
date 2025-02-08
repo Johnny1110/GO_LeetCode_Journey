5d1748d9383270ce1958c4746f44d13e8c1eebbc
Johnny.Wang <johnny.wang@btse.com>
2025-02-08 15:58:50 +0800

    feat: multiply_strings_revamp_2 done

--- medium/multiply_strings/revamp_2/template.go
+++ medium/multiply_strings/revamp_2/template.go
@@ -2,11 +2,41 @@ package multiply_strings_revamp_2
 
 import (
 	"go_leetcode_journey/common"
+	"strings"
 )
 
 func multiply(num1 string, num2 string) string {
-	// TODO: impl
-	return ""
+
+	if num1 == "0" || num2 == "0" {
+		return "0"
+	}
+
+	num1Len, num2Len := len(num1), len(num2)
+	// 1. create a result array (len = num1Len + numLen2)
+	result := make([]uint8, num1Len+num2Len)
+
+	// 2. iterate through the num1 and num2
+	for i := num1Len - 1; i >= 0; i-- {
+		for j := num2Len - 1; j >= 0; j-- {
+			product := (num1[i] - '0') * (num2[j] - '0')
+			temp := product + result[i+j+1]
+			result[i+j+1] = temp % 10
+			result[i+j] += temp / 10
+		}
+	}
+
+	// Convert result array to string (skip leading zeros)
+	var sb strings.Builder
+	skip := true
+	for _, digit := range result {
+		if digit == 0 && skip {
+			continue
+		}
+		skip = false
+		sb.WriteByte(byte(digit) + '0') // Convert int to byte
+	}
+
+	return sb.String()
 }
 
 // Go call this func in main.go
