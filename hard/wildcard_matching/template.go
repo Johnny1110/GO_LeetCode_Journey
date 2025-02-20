ae225916ac4329080223fed338d4f30cf7406084
Johnny.Wang <johnny.wang@btse.com>
2025-02-20 11:06:00 +0800

    init rust

--- hard/wildcard_matching/template.go
+++ hard/wildcard_matching/template.go
@@ -1,14 +1,37 @@
 package wildcard_matching
 
-import "go_leetcode_journey/common"
+import (
+	"fmt"
+	"go_leetcode_journey/common"
+)
 
-func isMatch(s string, p string) bool {
-	// TODO
+func isMatch(s string, pattern string) bool {
+	// init dp
+	dp := make([][]bool, len(s)+1)
+	for i := range dp {
+		dp[i] = make([]bool, len(pattern)+1)
+	}
+
+	// init dp[0][0] = True (empty s match empty pattern)
+	dp[0][0] = true
+	// init dp[i][0] = False
+
+	// dp[0][j] depends on whether p[0:j] consists only of '*'.
+	for j := 1; j <= len(pattern); j++ {
+		if pattern[j-1] == '*' && dp[0][j-1] {
+			dp[0][j] = true
+		}
+	}
+
+	fmt.Println(dp)
+
 	return false
 }
 
 // Go call this func in main.go
 func Go() {
+	common.Assert_answer(isMatch("", "*"), true)
+	common.Assert_answer(isMatch("", "***"), true)
 	common.Assert_answer(isMatch("aa", "a"), false)
 	common.Assert_answer(isMatch("a", "a"), true)
 	common.Assert_answer(isMatch("aa", "a?"), true)
