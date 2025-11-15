b3fe6ab26b7bc90e5560e9541cf09291635d89ca
Johnny.Wang <johnny.wang@btse.com>
2025-11-15 15:11:32 +0800

    feat: Word Break done

--- boost_up_training/dynamic_programming/word_break/README.md
+++ boost_up_training/dynamic_programming/word_break/README.md
@@ -170,5 +170,43 @@ Think about it this way:
 ### Coding - V2
 
 ```go
+func wordBreak(s string, wordDict []string) bool {
+	if len(s) == 0 {
+		return false
+	}
 
+	// 1. dump wordDict into a map
+	wordSet := make(map[string]bool)
+	wordLenSet := make(map[int]bool)
+	for _, word := range wordDict {
+		wordSet[word] = true
+		wordLenSet[len(word)] = true
+	}
+
+	// 2. init dp: dp[i] -> from s[0...i] can be broken into valid words.
+	dp := make([]bool, len(s)+1)
+	dp[0] = true // empty string is valid as default.
+
+	// example> s := "catsandog"
+
+	for i := 0; i < len(s)+1; i++ { // `i` is input s's index
+
+		for j := 0; j < i; j++ {
+
+			if dp[j] && wordLenSet[i-j] {
+				targetWord := s[j:i]
+				if wordSet[targetWord] {
+					dp[i] = true
+				}
+			}
+		}
+
+	}
+
+	return dp[len(s)]
+}
 ```
+
+Result:
+
+![2.png](imgs/2.png)
--- boost_up_training/dynamic_programming/word_break/word_break.go
+++ boost_up_training/dynamic_programming/word_break/word_break.go
@@ -1,25 +1,32 @@
 package word_break
 
 func wordBreak(s string, wordDict []string) bool {
-	// dump wordDict into a set, optimize finding word.
-	wordDictSet := make(map[string]bool)
-	for _, word := range wordDict {
-		wordDictSet[word] = true
+	if len(s) == 0 {
+		return false
 	}
 
-	// define DP
+	// 1. dump wordDict into a map
+	wordSet := make(map[string]bool)
+	wordLenSet := make(map[int]bool)
+	for _, word := range wordDict {
+		wordSet[word] = true
+		wordLenSet[len(word)] = true
+	}
+
+	// 2. init dp: dp[i] -> from s[0...i] can be broken into valid words.
 	dp := make([]bool, len(s)+1)
-	dp[0] = true // empty string always be true
+	dp[0] = true // empty string is valid as default.
 
-	// iterate through the s.
-	for currentDPIdx := 1; currentDPIdx < len(dp); currentDPIdx++ {
+	// example> s := "catsandog"
 
-		for j := 0; j <= currentDPIdx; j++ {
-			if dp[j] {
-				targetWord := s[j:currentDPIdx]
-				if _, ok := wordDictSet[targetWord]; ok {
-					dp[currentDPIdx] = true
-					break
+	for i := 0; i < len(s)+1; i++ { // `i` is input s's index
+
+		for j := 0; j < i; j++ {
+
+			if dp[j] && wordLenSet[i-j] {
+				targetWord := s[j:i]
+				if wordSet[targetWord] {
+					dp[i] = true
 				}
 			}
 		}
