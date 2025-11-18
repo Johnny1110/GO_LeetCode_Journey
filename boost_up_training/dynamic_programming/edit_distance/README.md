8b11d2bfbf4fc48fedc30211db5c77e17004cab2
Johnny.Wang <johnny.wang@btse.com>
2025-11-18 14:30:57 +0800

    feat: Edit Distance done

--- boost_up_training/dynamic_programming/edit_distance/README.md
+++ boost_up_training/dynamic_programming/edit_distance/README.md
@@ -117,6 +117,49 @@ example: "cat" to "cu"
 
 ```go
 func minDistance(word1 string, word2 string) int {
-    
+	// 1. define a 2D array:
+	dp := make([][]int, len(word2)+1)
+	for i := range dp {
+		dp[i] = make([]int, len(word1)+1)
+		dp[i][0] = i
+		// init dp:
+		if i == 0 {
+			for j := range dp[i] {
+				dp[i][j] = j
+			}
+		}
+	}
+
+	for i := 1; i < len(dp); i++ {
+		w2Idx := i - 1
+
+		for j := 1; j < len(dp[i]); j++ {
+			w1Idx := j - 1
+
+			if word1[w1Idx] == word2[w2Idx] {
+				// both are matched:
+				dp[i][j] = dp[i-1][j-1]
+			} else {
+				// both are not matched.
+
+				// 1. try replace:
+				replaceCnt := dp[i-1][j-1]
+				// 2. try insert:
+				insertCnt := dp[i-1][j]
+				// 3. try delete:
+				deleteCnt := dp[i][j-1]
+
+				dp[i][j] = min(replaceCnt, insertCnt, deleteCnt) + 1
+			}
+		}
+	}
+
+	return dp[len(word2)][len(word1)]
 }
 ```
+
+<br>
+
+Result:
+
+![1.png](imgs/1.png)
--- boost_up_training/dynamic_programming/edit_distance/edit_distance.go
+++ boost_up_training/dynamic_programming/edit_distance/edit_distance.go
@@ -1,9 +1,7 @@
 package edit_distance
 
-import "fmt"
-
 func minDistance(word1 string, word2 string) int {
-	// 1. define 2D array:
+	// 1. define a 2D array:
 	dp := make([][]int, len(word2)+1)
 	for i := range dp {
 		dp[i] = make([]int, len(word1)+1)
@@ -16,8 +14,6 @@ func minDistance(word1 string, word2 string) int {
 		}
 	}
 
-	fmt.Println("after init dp:", dp)
-
 	for i := 1; i < len(dp); i++ {
 		w2Idx := i - 1
 
@@ -26,24 +22,21 @@ func minDistance(word1 string, word2 string) int {
 
 			if word1[w1Idx] == word2[w2Idx] {
 				// both are matched:
-				dp[i][j] = dp[i-1][j-1] + 1
+				dp[i][j] = dp[i-1][j-1]
 			} else {
 				// both are not matched.
 
 				// 1. try replace:
 				replaceCnt := dp[i-1][j-1]
 				// 2. try insert:
-				insertCnt := dp[i][j-1]
+				insertCnt := dp[i-1][j]
 				// 3. try delete:
-				deleteCnt := dp[i-1][j]
+				deleteCnt := dp[i][j-1]
 
 				dp[i][j] = min(replaceCnt, insertCnt, deleteCnt) + 1
 			}
-
 		}
 	}
 
-	fmt.Println("dp:", dp)
-
-	return dp[len(word1)][len(word2)]
+	return dp[len(word2)][len(word1)]
 }
