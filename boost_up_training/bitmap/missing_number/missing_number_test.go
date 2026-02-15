43a564953cc32fc067e5252adf0601a6f9630900
Johnny.Wang <johnny.wang@btse.com>
2026-02-15 16:59:22 +0800

    missingNumber done

--- boost_up_training/bitmap/missing_number/missing_number_test.go
+++ boost_up_training/bitmap/missing_number/missing_number_test.go
@@ -1,16 +1,1 @@
 package missing_number
-
-import (
-	"testing"
-
-	"github.com/stretchr/testify/assert"
-)
-
-func Test_SumN(t *testing.T) {
-
-	res := sumN(4)
-	assert.Equal(t, 10, res)
-
-	res = sumN(3)
-	assert.Equal(t, 6, res)
-}
