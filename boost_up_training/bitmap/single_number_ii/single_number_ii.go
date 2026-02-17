d768759174ab6f94a595b22a56e15233d814f09b
Johnny.Wang <johnny.wang@btse.com>
2026-02-17 20:20:30 +0800

    Single Number II - can't handle negative value

--- boost_up_training/bitmap/single_number_ii/single_number_ii.go
+++ boost_up_training/bitmap/single_number_ii/single_number_ii.go
@@ -1,5 +1,33 @@
 package single_number_ii
 
+import "fmt"
+
 func singleNumber(nums []int) int {
-	return 0
+	counter := make([]int, 32) // int32 size
+
+	// 1. collect counter
+	for _, v := range nums {
+
+		fmt.Printf("inspect v: %v, byte(v):%b \n", v, v)
+		// each v is int32 -> 32 bit
+		for i := 0; i < 32; i++ {
+			mapping := 1 << i
+			if mapping&v != 0 {
+				counter[i] += 1
+			}
+		}
+	}
+
+	fmt.Printf("counter: %v \n", counter)
+
+	result := 0
+	// 2. find result in counter
+	for i, v := range counter {
+		if v%3 == 1 { // caught single 1
+			mapping := 1 << i
+			result = result | mapping
+		}
+	}
+
+	return result
 }
