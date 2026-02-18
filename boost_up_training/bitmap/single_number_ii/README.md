4597a9c7b15fc9c1b1ab05e239d2019450390b6b
Johnny.Wang <johnny.wang@btse.com>
2026-02-18 10:42:17 +0800

    Single Number II bitmap done

--- boost_up_training/bitmap/single_number_ii/README.md
+++ boost_up_training/bitmap/single_number_ii/README.md
@@ -87,6 +87,33 @@ count:  0  0  0  0  0  ... 0
 
 ```go
 func singleNumber(nums []int) int {
-    
+	counter := make([]int, 32) // int32 size
+
+	// 1. collect counter
+	for _, v := range nums {
+		// each v is int32 -> 32 bit
+		for i := 0; i < 32; i++ {
+			mapping := 1 << i
+			if mapping&v != 0 {
+				counter[i] += 1
+			}
+		}
+	}
+
+	var result int32
+	result = 0
+	// 2. find result in counter
+	for i, v := range counter {
+		if v%3 == 1 { // caught single 1
+			mapping := 1 << i
+			result = result | int32(mapping)
+		}
+	}
+
+	return int(result)
 }
 ```
+
+Result:
+
+![1](imgs/1.png)
--- boost_up_training/bitmap/single_number_ii/single_number_ii.go
+++ boost_up_training/bitmap/single_number_ii/single_number_ii.go
@@ -1,14 +1,10 @@
 package single_number_ii
 
-import "fmt"
-
 func singleNumber(nums []int) int {
 	counter := make([]int, 32) // int32 size
 
 	// 1. collect counter
 	for _, v := range nums {
-
-		fmt.Printf("inspect v: %v, byte(v):%b \n", v, v)
 		// each v is int32 -> 32 bit
 		for i := 0; i < 32; i++ {
 			mapping := 1 << i
@@ -18,16 +14,15 @@ func singleNumber(nums []int) int {
 		}
 	}
 
-	fmt.Printf("counter: %v \n", counter)
-
-	result := 0
+	var result int32
+	result = 0
 	// 2. find result in counter
 	for i, v := range counter {
 		if v%3 == 1 { // caught single 1
 			mapping := 1 << i
-			result = result | mapping
+			result = result | int32(mapping)
 		}
 	}
 
-	return result
+	return int(result)
 }
