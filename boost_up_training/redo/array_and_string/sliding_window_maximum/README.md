999977985e92656409e319ef9219d525f310c11e
Johnny.Wang <johnny.wang@btse.com>
2026-02-23 15:47:21 +0800

    redo: sliding windows maximum done

--- boost_up_training/redo/array_and_string/sliding_window_maximum/README.md
+++ boost_up_training/redo/array_and_string/sliding_window_maximum/README.md
@@ -11,6 +11,35 @@ link: https://leetcode.com/problems/sliding-window-maximum
 <br>
 <br>
 
+## Draft
+
+```
+Input: nums = [1,3,-1,-3,5,3,6,7], k = 3
+```
+
+define a queue: `[]int` which contains index of nums
+
+in first round, k = 3:
+
+```
+queue:
+
+put idx-0: [0]
+
+put idx-1: [0, 1]
+
+put idx-2, but nums[2] is lower than nums[1], so ignore it.: [0, 1]
+```
+
+We always keep current biggest num at the end of the queue.
+
+<br>
+
+In each round sliding-window is moving, we should expire from the end of queue.
+
+<br>
+<br>
+
 ## Coding-1
 
 ```go
--- boost_up_training/redo/array_and_string/sliding_window_maximum/sliding_window_maximum.go
+++ boost_up_training/redo/array_and_string/sliding_window_maximum/sliding_window_maximum.go
@@ -1,5 +1,118 @@
 package sliding_window_maximum
 
+// MonoQueue contains nums's index, biggest one always at the left
+type MonoQueue []int
+
+func (q MonoQueue) Len() int {
+	return len(q)
+}
+
+func (q *MonoQueue) PushRight(idx int) {
+	*q = append(*q, idx)
+}
+
+func (q *MonoQueue) PopLeft() (int, bool) {
+	if q.Len() == 0 {
+		return -1, false
+	}
+
+	idx := (*q)[0]
+	*q = (*q)[1:] // remove front
+
+	return idx, true
+}
+
+func (q *MonoQueue) PopRight() (int, bool) {
+	if q.Len() == 0 {
+		return -1, false
+	}
+
+	idx := (*q)[q.Len()-1]
+	*q = (*q)[:q.Len()-1]
+
+	return idx, true
+}
+
+func (q MonoQueue) PeekLeft() (int, bool) {
+	if q.Len() == 0 {
+		return -1, false
+	}
+
+	return q[0], true
+}
+
+func (q MonoQueue) PeekRight() (int, bool) {
+	if q.Len() == 0 {
+		return -1, false
+	}
+
+	return q[q.Len()-1], true
+}
+
+// ========================================================
+
 func maxSlidingWindow(nums []int, k int) []int {
-	return nil
+	result := make([]int, 0)
+
+	// init MonoQueue
+	Q := MonoQueue(make([]int, 0))
+
+	// init first window
+	for idx := 0; idx < k; idx++ {
+		if Q.Len() == 0 {
+			// nothing in queue, just push
+			Q.PushRight(idx)
+		} else {
+			// check left
+			for {
+				if rightIdx, exists := Q.PeekRight(); exists {
+					if nums[rightIdx] > nums[idx] {
+						Q.PushRight(idx)
+						break
+					} else {
+						Q.PopRight()
+					}
+				} else {
+					Q.PushRight(idx)
+					break
+				}
+			}
+		}
+	}
+
+	// collect frist result
+	leftIdx, _ := Q.PeekLeft()
+	result = append(result, nums[leftIdx])
+
+	// start sliding windows
+	for idx := k; idx < len(nums); idx++ {
+		// 1. check left, expire old idx
+		if leftIdx, exists := Q.PeekLeft(); exists {
+			if leftIdx+k-1 < idx { // expired
+				Q.PopLeft()
+			}
+		}
+
+		// 2. check right, remove small val
+		for {
+			if rightIdx, exists := Q.PeekRight(); exists {
+				if nums[rightIdx] > nums[idx] {
+					Q.PushRight(idx)
+					break
+				} else {
+					Q.PopRight()
+				}
+			} else {
+				Q.PushRight(idx)
+				break
+			}
+		}
+
+		// 3. collect result
+		leftIdx, _ := Q.PeekLeft()
+		result = append(result, nums[leftIdx])
+
+	}
+
+	return result
 }
