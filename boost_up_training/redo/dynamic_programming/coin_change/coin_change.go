f20b9d5a6a800a9dcb32f8e0c5cfb3a8406dae39
Johnny.Wang <johnny.wang@btse.com>
2026-02-28 13:43:22 +0800

    redo: coin change -2

--- boost_up_training/redo/dynamic_programming/coin_change/coin_change.go
+++ boost_up_training/redo/dynamic_programming/coin_change/coin_change.go
@@ -3,38 +3,43 @@ package coin_change
 import "math"
 
 func coinChange(coins []int, amount int) int {
-    if amount == 0 {
-        return 0
-    }
+	if amount == 0 {
+		return 0
+	}
 
-    dp := make([]int, amount+1)
+	dp := make([]int, amount+1)
 
-    for i := 1; i < amount+1; i++ {
-        
-        tmp := math.MaxInt32
+	for i := 1; i <= amount; i++ {
 
-        // iterate through the coins
-        for _, coin := range coins {
-            remainAmount := i - coin
+		currentBest := math.MaxInt32
 
-            if remainAmount == 0 {
-                tmp = 1
-                break // jack pock
-            } else if remainAmount > 0 {
+		for _, coin := range coins {
 
-                if dp[remainAmount] > 0 {
-                    tmp = min(1 + dp[remainAmount], tmp)
-                }
-            }
-        }
+			remainAmt := i - coin
 
-        if tmp == math.MaxInt32 {
-            dp[i] = -1
-        } else {
-             dp[i] = tmp
-        }
+			if remainAmt == 0 {
+				// spot on.
+				currentBest = 1
+				break
+			} else if remainAmt > 0 {
 
-    }
+				tmp := dp[remainAmt]
+				if tmp == -1 {
+					continue // no answer
+				}
 
-    return dp[amount]
+				currentBest = min(currentBest, tmp+1)
+
+			}
+		}
+
+		if currentBest == math.MaxInt32 {
+			dp[i] = -1
+		} else {
+			dp[i] = currentBest
+		}
+
+	}
+
+	return dp[amount]
 }
