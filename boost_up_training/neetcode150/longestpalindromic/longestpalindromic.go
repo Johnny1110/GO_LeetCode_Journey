da53afd48c9838ce1e92726d79c759729a2e76ce
Johnny Wang <Jarvan1110@email.com>
2026-03-15 19:31:02 +0800

    neetcode: init longestpalindromic

--- boost_up_training/neetcode150/longestpalindromic/longestpalindromic.go
+++ boost_up_training/neetcode150/longestpalindromic/longestpalindromic.go
@@ -3,7 +3,11 @@ package longestpalindromic
 func longestPalindrome(s string) string {
 
 	// define DP -> index: palindromic len, value
-	dp = make([][]bool, 0) // i ~ j (from idx-i to idx-j is palindrome)
+	dp := make([][]bool, len(s)) // i ~ j (from idx-i to idx-j is palindrome)
+	for idx := 0; idx < len(dp); idx++ {
+		dp[idx] = make([]bool, len(s))
+		dp[idx][idx] = true
+	}
 
 	return ""
 }
