2a676d7909685311246e85e71d77366f65518c79
Johnny.Wang <johnny.wang@btse.com>
2026-03-30 18:55:45 +0800

    feat: init 33. Search in Rotated Sorted Array

---
+++ boost_up_training/other/33.search-in-rotated-sorted-array.go
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode id=33 lang=golang
+ *
+ * [33] Search in Rotated Sorted Array
+ */
+
+// @lc code=start
+func search(nums []int, target int) int {
+
+	left, right := 0, len(nums)-1
+	mid := (left + right) / 2
+
+}
+
+// @lc code=end
+
