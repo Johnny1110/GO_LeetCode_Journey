4a84dfb5634174e1a86d1bb97887c745ebcbbb78
Johnny.Wang <johnny.wang@btse.com>
2026-04-01 17:25:39 +0800

    sql: LC 1084. Sales Analysis III

--- sql_training/sales_analysis_iii/answer.sql
+++ sql_training/sales_analysis_iii/answer.sql
@@ -1,1 +1,16 @@
--- 1. 
+-- 1. my answer: subquery with no in ().
+SELECT DISTINCT s.product_id, p.product_name
+FROM Sales s
+         JOIN Product p ON p.product_id = s.product_id
+WHERE s.product_id NOT IN (
+    SELECT product_id
+    FROM Sales
+    WHERE sale_date < '2019-01-01' OR sale_date > '2019-03-31'
+);
+
+--2. std answer:
+SELECT p.product_id, p.product_name
+FROM Product p
+         JOIN Sales s ON p.product_id = s.product_id
+GROUP BY p.product_id, p.product_name
+HAVING MIN(s.sale_date) >= '2019-01-01' AND MAX(s.sale_date) <= '2019-03-31';
